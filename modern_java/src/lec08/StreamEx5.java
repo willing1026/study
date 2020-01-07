a5314c50f7ddeb19470fed6f9bc7b4cf46274c31
jaeseon_hwang <jshwang@deleo.co.kr>
2020-01-08 08:52:06 +0900

    #### 모던 자바 (자바8) 못다한 이야기 - 08 Stream API - 03 Stream API 05 - Parallel Programming (with ParallelStream)

--- modern_java/src/lec08/StreamEx5.java
+++ modern_java/src/lec08/StreamEx5.java
@@ -10,30 +10,63 @@ public class StreamEx5 {
         final int[] sum = {0};
 
         //single core programing
-        IntStream.range(0, 100)
-                .forEach(i -> sum[0] += i);
-        System.out.println("sum: " + sum[0]);
+//        IntStream.range(0, 100)
+//                .forEach(i -> sum[0] += i);
+//        System.out.println("sum: " + sum[0]);
+//
+//        //parallel programing
+//        final int[] sum2 = {0};
+//        IntStream.range(0, 100)
+//                .parallel()
+//                .forEach(i -> sum2[0] += i);
+//
+//        System.out.println("parallel sum(with side-effect): " + sum2[0]);
+//
+//        System.out.println("stream sum(without side-effect): " +
+//                IntStream.range(0, 100)
+//                        .sum());
+//
+//        System.out.println("parallel stream sum(without side-effect): " +
+//                IntStream.range(0, 100)
+//                        .parallel()
+//                        .sum());
+//
+//        //parallel stream 장점
+//        final long start = System.currentTimeMillis();
+//        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+//                .parallelStream()
+//                .map(i -> {
+//                    try {
+//                        TimeUnit.SECONDS.sleep(1);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    return i;
+//                })
+//                .forEach(i -> System.out.println(i));
+//        System.out.println("parallel stream time : " + (System.currentTimeMillis() - start));
+//
+//        final long start2 = System.currentTimeMillis();
+//        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+//                .stream()
+//                .map(i -> {
+//                    try {
+//                        TimeUnit.SECONDS.sleep(1);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    return i;
+//                })
+//                .forEach(i -> System.out.println(i));
+//        System.out.println("single stream time : " + (System.currentTimeMillis() - start2));
 
-        //parallel programing
-        final int[] sum2 = {0};
-        IntStream.range(0, 100)
-                .parallel()
-                .forEach(i -> sum2[0] += i);
 
-        System.out.println("parallel sum(with side-effect): " + sum2[0]);
-
-        System.out.println("stream sum(without side-effect): " +
-                IntStream.range(0, 100)
-                        .sum());
-
-        System.out.println("parallel stream sum(without side-effect): " +
-                IntStream.range(0, 100)
-                        .parallel()
-                        .sum());
-
-        //parallel stream 장점
-        final long start = System.currentTimeMillis();
-        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+        //core 개수 설정
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "3"); // 0 ~ 3 : 4개
+        final long start3 = System.currentTimeMillis();
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
                 .parallelStream()
                 .map(i -> {
                     try {
@@ -45,26 +78,7 @@ public class StreamEx5 {
                     return i;
                 })
                 .forEach(i -> System.out.println(i));
-        System.out.println("parallel stream time : " + (System.currentTimeMillis() - start));
-
-        final long start2 = System.currentTimeMillis();
-        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
-                .stream()
-                .map(i -> {
-                    try {
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    return i;
-                })
-                .forEach(i -> System.out.println(i));
-        System.out.println("single stream time : " + (System.currentTimeMillis() - start2));
-
-
-        //core 개수 설정
-        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "7");
+        System.out.println("parallelism stream time : " + (System.currentTimeMillis() - start3));
     }
 
 }
