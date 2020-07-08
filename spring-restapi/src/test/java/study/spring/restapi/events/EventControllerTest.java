a80f507009df5abece44b5f0ed219eade601a011
jaeseon_hwang <jshwang@deleo.co.kr>
2020-07-09 08:33:15 +0900

    201 응답 받기

--- spring-restapi/src/test/java/study/spring/restapi/events/EventControllerTest.java
+++ spring-restapi/src/test/java/study/spring/restapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package study.spring.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -19,13 +23,31 @@ public class EventControllerTest {
 	@Autowired
 	MockMvc mockMvc;
 
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
 	void createEvent() throws Exception {
+		Event event = Event.builder()
+				.name("Spring")
+				.description("REST API")
+				.beginEventDateTime(LocalDateTime.of(2020, 7, 8, 17, 58))
+				.closeEnrollmentDateTime(LocalDateTime.of(2020, 7, 8, 18, 58))
+				.beginEventDateTime(LocalDateTime.of(2020, 7, 10, 9, 0))
+				.endEventDateTime(LocalDateTime.of(2020, 7, 10, 18, 0))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("강남역 D2")
+				.build();
+
 		mockMvc.perform(post("/api/events/")
 				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(event))
 				.accept(MediaTypes.HAL_JSON)
 		)
 				.andDo(print())
-				.andExpect(status().isCreated());
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("id").exists());
 	}
 }
